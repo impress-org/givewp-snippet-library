1a3b9a7a5ce2c6789e834764dbbbe13b8ef94f2c
devin <devin@wordimpress.com>
2015-12-07 12:34:45 -0800

    Adjusted custom donation code comments

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Give Snippet Library
-A collection of code snippets to enhance or expand on the free Give WordPress Donation Plugin. 
+
+A collection of code snippets to enhance or expand on the free Give WordPress Donation Plugin. Be sure to customize the function names or wrap with `function_exists` contionals to avoid collisions. 
 
 We accept PR's of contributions.
--- form-customizations/custom-donation-total-label.php
+++ form-customizations/custom-donation-total-label.php
@@ -2,13 +2,14 @@
 /**
  * Give Custom Donation Total Label
  *
- * @description: Customizes the "Donation Total" label for all Give forms; be sure to customize the name of the function to avoid conflicts
+ * @description: Customizes the "Donation Total" label for all Give forms; be sure to customize the name of the function to avoid conflicts; customize textdomain to your site's if you want to translate the string in various languages
  *
  * @return string|void
  */
 function my123_custom_donation_total_label() {
 
 	return __( 'My Custom Label', 'my_textdomain' );
+
 
 }
 
