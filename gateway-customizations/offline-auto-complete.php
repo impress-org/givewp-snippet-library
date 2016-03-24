7dd9170cc2d24aa3581a0d823883471df1330208
Devin Walker <devin@wordimpress.com>
2016-03-23 20:22:57 -0700

    New snippet for auto approving offline donations - they never go to a pending state

---
+++ gateway-customizations/offline-auto-complete.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Give Auto Approve Offline Donations
+ *
+ * @description: If you would like to have donations skip the "pending" stage and auto-approve
+ *
+ * @param $payment_data array
+ */
+function give_auto_approve_offline_donations( $payment_args, $payment_data ) {
+
+	//Ensure gateway is set and check for offline gateway
+	if ( ! isset( $payment_data['gateway'] ) || $payment_data['gateway'] !== 'offline' ) {
+		//Passthrough
+		return $payment_args;
+	}
+
+	//Only pending donations
+	if ( isset( $payment_args['post_status'] ) && $payment_args['post_status'] === 'pending' ) {
+		//Set post_status to complete / publish
+		$payment_args['post_status'] = 'publish';
+	}
+
+	return $payment_args;
+
+}
+
+add_filter( 'give_insert_payment_args', 'give_auto_approve_offline_donations', 999, 2 );
