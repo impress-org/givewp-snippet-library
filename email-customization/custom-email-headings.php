45982442ab595614a623458a70b9bac027d3b3cd
Devin Walker <devin@wordimpress.com>
2016-04-21 10:59:28 -0700

    New snippet for customizing the email headings

---
+++ email-customization/custom-email-headings.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Customize Give's Email Headings
+ *
+ * @description: Use this snippet to update the donation email's main headings; for instance "New Donation!", etc.
+ *
+ * @param $message
+ * @param $email_obj
+ */
+function my_custom_give_email_headings( $message ) {
+
+	switch ( $message ) {
+
+		case 'New Donation!':
+			$message = 'Boom! New Donation';
+			break;
+		case 'Donation Receipt':
+			$message = 'Your Donation Receipt';
+			break;
+
+	}
+
+	return $message;
+
+}
+
+add_filter( 'give_email_heading', 'my_custom_give_email_headings', 10, 1 );
