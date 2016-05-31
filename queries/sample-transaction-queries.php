ed8bec849af07248e236c05101b2c4407a4678c6
Devin Walker <devin@wordimpress.com>
2016-05-30 19:42:38 -0700

    Reformatted code

--- queries/sample-transaction-queries.php
+++ queries/sample-transaction-queries.php
@@ -25,16 +25,16 @@ get_header();
 		$testing = false;
 
 		if ( $testing == true ) {
-		$testquery = new WP_Query( array( 'post_type' => 'give_payment', 'post_count' => 1 ) );
-		$firstpost = $testquery->posts;
-		$meta = get_post_meta( $firstpost[0]->ID );
+			$testquery = new WP_Query( array( 'post_type' => 'give_payment', 'post_count' => 1 ) );
+			$firstpost = $testquery->posts;
+			$meta      = get_post_meta( $firstpost[0]->ID );
 
 			?>
-		<div style="background: #555; color: white; padding: 2rem;">
-			<h3>Test Data</h3>
-			<p>The following outputs all the "give_payment" fields for you to reference in building out your donation list</p>
-			<p><?php var_dump($meta);?></p>
-		</div>
+			<div style="background: #555; color: white; padding: 2rem;">
+				<h3>Test Data</h3>
+				<p>The following outputs all the "give_payment" fields for you to reference in building out your donation list</p>
+				<p><?php var_dump( $meta ); ?></p>
+			</div>
 		<?php } // end testing ?>
 
 		<?php
@@ -47,7 +47,7 @@ get_header();
 
 		// Query 1 Argument
 		$args = array(
-			'post_type' => 'give_payment',
+			'post_type'      => 'give_payment',
 			'posts_per_page' => 3
 		);
 
@@ -55,133 +55,139 @@ get_header();
 
 		if ( $loop->have_posts() ) : ?>
 
-		<h2>Output latest 3 donations with amount and date</h2>
-		<hr />
-		<ul>
-		<?php
-			while ( $loop->have_posts() ) : $loop->the_post();
-				$meta = get_post_meta( get_the_ID() );
-				$total = $meta['_give_payment_total'][0];
-				$getdate = $meta['_give_completed_date'][0];
-				$date = date("F j, Y", strtotime($getdate));
-				$gateway = $meta['_give_payment_gateway'][0];
-		?>
+			<h2>Output latest 3 donations with amount and date</h2>
+			<hr/>
+			<ul>
+				<?php
+				while ( $loop->have_posts() ) : $loop->the_post();
+					$meta    = get_post_meta( get_the_ID() );
+					$total   = $meta['_give_payment_total'][0];
+					$getdate = $meta['_give_completed_date'][0];
+					$date    = date( "F j, Y", strtotime( $getdate ) );
+					$gateway = $meta['_give_payment_gateway'][0];
+					?>
 
-			<li><strong>Donation for $<?php echo $total; ?></strong><br />
-				Was given on <?php echo $date; ?><br />
-				With the <?php echo $gateway; ?> Payment Gateway
-			</li>
+					<li><strong>Donation for $<?php echo $total; ?></strong><br/>
+						Was given on <?php echo $date; ?><br/>
+						With the <?php echo $gateway; ?> Payment Gateway
+					</li>
 
-		<?php	endwhile; wp_reset_postdata(); // end of Query 1 ?>
-		</ul>
+				<?php endwhile;
+				wp_reset_postdata(); // end of Query 1 ?>
+			</ul>
 		<?php else : ?>
-		<!-- If you don't have donations that fit this query -->
+			<!-- If you don't have donations that fit this query -->
 
-		<h2>Sorry you don't have any transactions that fit this query</h2>
+			<h2>Sorry you don't have any transactions that fit this query</h2>
 
-		<?php endif; wp_reset_query(); ?>
+		<?php endif;
+		wp_reset_query(); ?>
 
-	<?php
-	/**
-	 *  QUERY 2: Query by Gateway
-	 *
-	 *  This query outputs the latest 3 transactions
-	 *  that were made with the Offline Donations Gateway only
-	 */
-
-	// Query 2 Arguments
-	$args2 = array(
-		'post_type' => 'give_payment',
-		'posts_per_page' => 3,
-		'meta_key'     => '_give_payment_gateway',
-		'meta_value'   => 'manual',
-		'meta_compare' => '=',
-	);
-
-	$loop2 = new WP_Query( $args2 );
-
-	if ( $loop2->have_posts() ) : ?>
-
-	<h2>Output latest 3 Offline donations with amount and email address</h2>
-	<hr />
-	<ul>
-	<?php
-		while ( $loop2->have_posts() ) : $loop2->the_post();
-			$meta = get_post_meta( get_the_ID() );
-			$total = $meta['_give_payment_total'][0];
-			$getdate = $meta['_give_completed_date'][0];
-			$date = date("F j, Y", strtotime($getdate));
-			$gateway = $meta['_give_payment_gateway'][0];
-		?>
-		<li><strong>Donation for $<?php echo $total; ?></strong><br />
-			Was given on <?php echo $date; ?><br />
-			With the <?php echo $gateway; ?> Payment Gateway
-		</li>
-
-		<?php	endwhile; wp_reset_postdata(); // end of Query 1 ?>
-	</ul>
-
-	<?php else : ?>
-		<!-- If you don't have donations that fit this query -->
-		<h2>Sorry you don't have any transactions that fit this query</h2>
-
-		<?php endif; wp_reset_query(); ?>
-
-<?php
-/**
- *  QUERY 3: Show Donor Information
- *
- *  This query outputs the latest 3 transactions
- *  with the name of the donor and the amount
- */
-
-// Query 3 Arguments
-$args3 = array(
-	'post_type' => 'give_payment',
-	'posts_per_page' => 3,
-);
-
-$loop3 = new WP_Query( $args3 );
-
-if ( $loop3->have_posts() ) : ?>
-
-	<h2>Output latest 3 donations with amount and names</h2>
-	<hr />
-	<ul>
-	<?php
-		/** Getting user data is a bit more complex
-		 *  Also keep in mind whether or not your donors
-		 *  actually WANT their names posted publicly.
+		<?php
+		/**
+		 *  QUERY 2: Query by Gateway
+		 *
+		 *  This query outputs the latest 3 transactions
+		 *  that were made with the Offline Donations Gateway only
 		 */
 
-		while ( $loop3->have_posts() ) : $loop3->the_post();
+		// Query 2 Arguments
+		$args2 = array(
+			'post_type'      => 'give_payment',
+			'posts_per_page' => 3,
+			'meta_key'       => '_give_payment_gateway',
+			'meta_value'     => 'manual',
+			'meta_compare'   => '=',
+		);
 
-			$meta = get_post_meta( get_the_ID() );
-			// Transaction have their own metadata; let's get it.
-			$paymentmeta = $meta['_give_payment_meta'];
+		$loop2 = new WP_Query( $args2 );
 
-			// The metadata is serialized. Let's pull that apart.
-			$getmeta = maybe_unserialize($paymentmeta[0]);
+		if ( $loop2->have_posts() ) : ?>
 
-			// Now that we've got it, we can define the name
-			$firstname = $getmeta['user_info']['first_name'];
-			$lastname = $getmeta['user_info']['last_name'];
+			<h2>Output latest 3 Offline donations with amount and email address</h2>
+			<hr/>
+			<ul>
+				<?php
+				while ( $loop2->have_posts() ) : $loop2->the_post();
+					$meta    = get_post_meta( get_the_ID() );
+					$total   = $meta['_give_payment_total'][0];
+					$getdate = $meta['_give_completed_date'][0];
+					$date    = date( "F j, Y", strtotime( $getdate ) );
+					$gateway = $meta['_give_payment_gateway'][0];
+					?>
+					<li><strong>Donation for $<?php echo $total; ?></strong><br/>
+						Was given on <?php echo $date; ?><br/>
+						With the <?php echo $gateway; ?> Payment Gateway
+					</li>
 
-			$total = $meta['_give_payment_total'][0];
-	?>
+				<?php endwhile;
+				wp_reset_postdata(); // end of Query 1 ?>
+			</ul>
 
-		<li>
-			<strong>Thanks to <?php echo $firstname . ' ' . $lastname; ?></strong><br />
-				For their generous gift of $<?php echo $total; ?><br />
-		</li>
-		<?php	endwhile; wp_reset_postdata(); // end of Query 1 ?>
-		</ul>
 		<?php else : ?>
-		<!-- If you don't have donations that fit this query -->
-		<h2>Sorry you don't have any transactions that fit this query</h2>
+			<!-- If you don't have donations that fit this query -->
+			<h2>Sorry you don't have any transactions that fit this query</h2>
 
-		<?php endif; wp_reset_query(); ?>
-</main>
+		<?php endif;
+		wp_reset_query(); ?>
+
+		<?php
+		/**
+		 *  QUERY 3: Show Donor Information
+		 *
+		 *  This query outputs the latest 3 transactions
+		 *  with the name of the donor and the amount
+		 */
+
+		// Query 3 Arguments
+		$args3 = array(
+			'post_type'      => 'give_payment',
+			'posts_per_page' => 3,
+		);
+
+		$loop3 = new WP_Query( $args3 );
+
+		if ( $loop3->have_posts() ) : ?>
+
+			<h2>Output latest 3 donations with amount and names</h2>
+			<hr/>
+			<ul>
+				<?php
+				/** Getting user data is a bit more complex
+				 *  Also keep in mind whether or not your donors
+				 *  actually WANT their names posted publicly.
+				 */
+
+				while ( $loop3->have_posts() ) : $loop3->the_post();
+
+					$meta = get_post_meta( get_the_ID() );
+					// Transaction have their own metadata; let's get it.
+					$paymentmeta = $meta['_give_payment_meta'];
+
+					// The metadata is serialized. Let's pull that apart.
+					$getmeta = maybe_unserialize( $paymentmeta[0] );
+
+					// Now that we've got it, we can define the name
+					$firstname = $getmeta['user_info']['first_name'];
+					$lastname  = $getmeta['user_info']['last_name'];
+
+					$total = $meta['_give_payment_total'][0];
+					?>
+
+					<li>
+						<strong>Thanks to <?php echo $firstname . ' ' . $lastname; ?></strong><br/>
+						For their generous gift of $<?php echo $total; ?><br/>
+					</li>
+				<?php endwhile;
+				wp_reset_postdata(); // end of Query 1 ?>
+			</ul>
+		<?php else : ?>
+			<!-- If you don't have donations that fit this query -->
+			<h2>Sorry you don't have any transactions that fit this query</h2>
+
+		<?php endif;
+		wp_reset_query(); ?>
+	</main>
 
 </div>
 
