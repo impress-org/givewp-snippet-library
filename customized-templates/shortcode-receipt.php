6e727d17cb7e46230ab4b0710f1ae59aa4d32f1a
Matt Cromwell <reachme@mattcromwell.com>
2016-06-10 11:09:49 -0700

    Removed "TEST" text

--- customized-templates/shortcode-receipt.php
+++ customized-templates/shortcode-receipt.php
@@ -23,7 +23,6 @@ $status   = give_get_payment_status( $payment, true );
 ?>
 
 <?php do_action( 'give_payment_receipt_before_table', $payment, $give_receipt_args ); ?>
-<h3>TEST!</h3>
 	<table id="give_donation_receipt" class="give-table">
 		<thead>
 		<?php do_action( 'give_payment_receipt_before', $payment, $give_receipt_args ); ?>
