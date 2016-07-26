2d475fbeb7d42d152cdf0d795aaa2f859dd2c1dc
Ben Meredith <ben.meredith@gmail.com>
2016-07-26 14:31:28 -0400

    Adds additional gateways to the list, clarifies comments

--- gateway-customizations/customize-payment-gateway-labels.php
+++ gateway-customizations/customize-payment-gateway-labels.php
@@ -1,23 +1,67 @@
 <?php
 /**
- * Customize the Gateways Labels
+ * Customize the Gateway Labels
  *
- * @description: This function uses the `give_payment_gateways` library and adjusts the label that appears for the donor on the frontend donation forms. This will affect ALL donation forms.
+ * @description: This function uses the `give_payment_gateways` library and adjusts 
+ * the label that appears for the donor on the frontend donation forms. This will affect ALL donation forms.
+ * The 'admin_label' will affect the backend of the site, while the 'checkout_label' will affect the donor-facing labels
+ * 
+ * Note: only use the 4 lines following the commented line for the gateway labels you wish to modify. 
+ * You may safely delete the rest. We strongly recommend testing this code before use on production sites.
  *
  * @param $gateways
  *
  * @return mixed
  */
 function my_custom_gateway_labels($gateways) {
+	// add the following 4 lines to change the label for Offline Donations
 	$gateways['offline'] = array(
-		'admin_label'    => 'Offline Donations',
+		'admin_label'    => 'Offline Donation',
 		'checkout_label' => __( 'Mail a Check', 'give' )
 	);
+	
+	// add the following 4 lines to change the label for PayPal Standard 
 	$gateways['paypal'] = array(
 		'admin_label'    => 'PayPal Standard',
 		'checkout_label' => __( 'Credit/Debit Card or PayPal', 'give' )
 	);
-
+	
+	// add the following 4 lines to change the label for Stripe
+	$gateways['stripe'] = array(
+		'admin_label'    => 'Stripe',
+		'checkout_label' => __( 'Credit Card (Stripe)', 'give' )
+	);
+	
+	// add the following 4 lines to change the label for Braintree
+	$gateways['braintree'] = array(
+		'admin_label'    => 'Braintree',
+		'checkout_label' => __( 'Credit Card (Braintree)', 'give' )
+	);
+	
+	// add the following 4 lines to change the label for Dwolla
+	$gateways['dwolla'] = array(
+		'admin_label'    => 'Dwolla',
+		'checkout_label' => __( 'Dwolla Account', 'give' )
+	);
+	
+	// add the following 4 lines to change the label for Authorize.net
+	$gateways['authorize'] = array(
+		'admin_label'    => 'Authorize.net',
+		'checkout_label' => __( 'Credit Card (Authorize.net)', 'give' )
+	);
+	
+	// add the following 4 lines to change the label for Paymill
+	$gateways['paymill'] = array(
+		'admin_label'    => 'Paymill',
+		'checkout_label' => __( 'Credit Card (Paymill)', 'give' )
+	);
+	
+	// add the following 4 lines to change the label for 2Checkout
+	$gateways['twocheckout'] = array(
+		'admin_label'    => '2Checkout',
+		'checkout_label' => __( 'Credit Card (2Checkout)', 'give' )
+	);
+	
     return $gateways;
 }
 
