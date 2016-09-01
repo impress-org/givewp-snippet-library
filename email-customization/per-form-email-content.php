974f668b1bfd601f37dcdf729688109e56113aab
Ben Meredith <ben.meredith@gmail.com>
2016-09-01 12:50:31 -0400

    Add a help to customize for two different forms.

--- email-customization/per-form-email-content.php
+++ email-customization/per-form-email-content.php
@@ -27,6 +27,18 @@ function my_custom_give_email_content( $email_body, $payment_id, $payment_data )
 		//Make sure we return the new $email_body;
 		return $email_body;
 
+	} elseif ( $form_id == '460' ) {
+
+		//Here you can output custom content or pull from custom post meta using get_post_meta, or use an ACF field, WordPress functions, etc.
+		//You can also use Give's email tags like {price}, {fullname}, etc.
+		$email_body = '<p>Hi {name},</p>';
+		$email_body .= '<p>Thank you for your donation to . ' . get_the_title( $form_id ) . '. This is some different custom content!</p>';
+		$email_body .= '<p>Some different content...</p>';
+		$email_body .= '<p>Even our custom email tags: {receipt_link}</p>';
+
+		//Make sure we return the new $email_body;
+		return $email_body;
+
 	} else {
 		//Be sure to return default email body if we don't make a match
 		return $email_body;
