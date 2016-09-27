77fdd76c2526491ebfaafd6209c2d8971090bcac
Devin Walker <devin@wordimpress.com>
2016-09-27 11:03:05 -0700

    New snippet to  Prevent the donor notification email but keep the admin notification email functionality.

---
+++ email-customization/disable-donor-notification.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Prevent the donor notification email but keep the admin notification email functionality.
+ *
+ * @param $payment_id
+ */
+function my123_give_remove_donor_notification( $payment_id ) {
+	remove_action( 'give_complete_purchase', 'give_trigger_donation_receipt', 999, 1 );
+
+	//Remove these lines to stop triggering the admin notification.
+	$payment_data = give_get_payment_meta( $payment_id );
+
+	if ( ! give_admin_notices_disabled( $payment_id ) ) {
+		do_action( 'give_admin_sale_notice', $payment_id, $payment_data );
+	}
+
+}
+
+add_action( 'give_complete_purchase', 'my123_give_remove_donor_notification', 1 );
