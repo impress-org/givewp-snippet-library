a48410b88adb2c769aa5d404b51beb80de01d30e
Matt Cromwell <reachme@mattcromwell.com>
2016-10-04 14:40:00 -0700

    Add recent-donors-shortcode to "useful-queries" folder

---
+++ useful-queries/recent-donors-shortcode.php
@@ -0,0 +1,39 @@
+<?php
+/**
+  *  Shortcode to list recent donors  
+  *
+  */
+ 
+function recent_donors_function() {
+    
+    //Get the latest 100 Give Donors
+    $args = array(
+        'number' => 100,
+    );
+
+    $donors = Give()->customers->get_customers($args);
+    foreach($donors as $donor) {
+        
+        $output = $donor->name.", ";
+        // First and Last Name
+        $name = $donor->name;
+
+        //Split up the names
+        $separate = explode(" ", $name);
+
+        //find the surname
+        $last = array_pop($separate);
+
+        //Shorten up the name so it's Jason T.  instead of Jason Tucker
+        $shortenedname = implode(' ', $separate). " ".$last[0]. ".";
+
+        //Display the Jason T. and include a , after it.
+        $output .= $shortenedname . ", ";
+
+    }
+    $output .= " and many more.";
+    return $output;
+
+}
+
+add_shortcode('donor_list', 'recent_donors_function');
