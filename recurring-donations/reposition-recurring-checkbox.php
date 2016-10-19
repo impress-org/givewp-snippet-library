c495a7619ccbbb7bbbc0d27914dfff8a68672ef0
Devin Walker <devin@wordimpress.com>
2016-10-19 12:18:22 -0700

    Additional text to clarify language remove_action only takes 3 params

--- recurring-donations/reposition-recurring-checkbox.php
+++ recurring-donations/reposition-recurring-checkbox.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * Customize the Recurring Checkbox
+ * Customize the Recurring Checkbox location for ALL forms.
  *
  * Important - rename this function so it's unique to avoid conflicts.
  *
- * @description: This function unhooks Give's checkbox output action and then sets it to a desired location (see https://givewp.com/documentation/developers/actions-explanation-and-usage/ for all available actions) within the donation form using a separate action.
+ * This function unhooks Give's checkbox output action and then sets it to a desired location (see https://givewp.com/documentation/developers/actions-explanation-and-usage/ for all available actions) within the donation form using a separate action.
  */
 function my_customize_recurring_checkbox_location(){
 
-	//First we remove the action
-	remove_action( 'give_after_donation_levels', 'give_output_donors_choice_checkbox', 10, 2 );
+	//First we remove the action.
+	remove_action( 'give_after_donation_levels', 'give_output_donors_choice_checkbox', 10 );
 
 
-	//Then we re-add it to our desired location
+	//Then we re-add it to our desired location.
 	add_action( 'give_before_donation_levels', 'give_output_donors_choice_checkbox', 10, 2 );
 
 
