795087ce743c34a287486904fa7a9be048bb03bb
Devin Walker <devin@wordimpress.com>
2016-10-25 17:12:53 -0700

    Renamed files to better represent the functionality within

--- form-customizations/conditional-successful-donation-redirect.php
+++ form-customizations/conditional-successful-donation-redirect.php
@@ -1,28 +1,31 @@
-<?php 
+<?php
 /**
  * Redirect to Custom Page on Successful Donation
- * 
+ *
  * In this example, form ID 7 will redirect after successful donation to page/post ID 20,
- * and form ID 230 will redirect after sucessful donation to https://example.com.
- * 
+ * and form ID 230 will redirect after successful donation to https://example.com.
+ *
  * For help finding IDs, check out this tutorial:
  * http://www.wpbeginner.com/beginners-guide/how-to-find-post-category-tag-comments-or-user-id-in-wordpress/
  *
  * @return string
  * @author support@givewp.com
+ *
+ * @param $success_page
+ *
+ * @return string
  */
-
 function my_custom_give_redirects( $success_page ) {
 
-    $form_id = isset( $_POST['give-form-id'] ) ? $_POST['give-form-id'] : 0;
+	$form_id = isset( $_POST['give-form-id'] ) ? $_POST['give-form-id'] : 0;
 
-    if ( $form_id == 7 ) {
-        $success_page = esc_url( get_permalink( 20 ) );
-    } elseif ( $form_id == 230 ) {
-        $success_page = esc_url( 'https://example.com' );
-    }
+	if ( $form_id == 7 ) {
+		$success_page = esc_url( get_permalink( 20 ) );
+	} elseif ( $form_id == 230 ) {
+		$success_page = esc_url( 'https://example.com' );
+	}
 
-    return $success_page;
+	return $success_page;
 
 }
 
