944a294e97c6bb506e75fd1e4727dd210674c9a7
Ben Meredith <ben.meredith@gmail.com>
2017-02-22 13:34:26 -0500

    Update stripe-pass-custom-meta-for-customers.php

--- gateway-customizations/stripe-pass-custom-meta-for-customers.php
+++ gateway-customizations/stripe-pass-custom-meta-for-customers.php
@@ -5,9 +5,6 @@
  * Retrieves custom form field data from the $_POST variable and merges them into
  * the metadata array that is passed to Stripe when a new customer is created.
  *
- * Note that Form field manager adds a prefix to the meta key of ffm- so the meta key you see 
- * on the back end of Form Field Manager's interface will need to be prepended with ffm- to function.
- *
  * @param $metadata array An array of default meta data.
  *
  * @return array
@@ -15,9 +12,9 @@
 function my_give_custom_stripe_meta( $metadata ) {
 
 	$custom_meta_fields = array(
-		'ffm-custom_field_1' => isset( $_POST['ffm-custom_field_meta_key_1'] ) ? $_POST['ffm-custom_field_meta_key_1'] : '',
-		'ffm-custom_field_2' => isset( $_POST['ffm-custom_field_meta_key_2'] ) ? $_POST['ffm-custom_field_meta_key_2'] : '',
-		'ffm-custom_field_3' => isset( $_POST['ffm-custom_field_meta_key_3'] ) ? $_POST['ffm-custom_field_meta_key_3'] : '',
+		'custom_field_1' => isset( $_POST['custom_field_meta_key_1'] ) ? $_POST['custom_field_meta_key_1'] : '',
+		'custom_field_2' => isset( $_POST['custom_field_meta_key_2'] ) ? $_POST['custom_field_meta_key_2'] : '',
+		'custom_field_3' => isset( $_POST['custom_field_meta_key_3'] ) ? $_POST['custom_field_meta_key_3'] : '',
 	);
 
 	$metadata = array_merge( $metadata, $custom_meta_fields );
