f675f0c49c18c15037ad2349b1de819c582b542b
Devin Walker <devin@wordimpress.com>
2017-04-13 12:02:52 -0700

    1. Revised how the engraving fields is saved moving it from the payment_meta array to it’s own custom meta field. 2. Updated how field is validated to user proper give hook 3. Revised the email tag to be more semantic with underscore matching field value 4. General code improvement and best practices implementation

--- form-customizations/custom-fields-plugin.php
+++ form-customizations/custom-fields-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Give - Example Custom Field Added.
+ * Plugin Name: Give - Example Custom Field Integration
  * Plugin URI: https://givewp.com/documentation/developers/how-to-create-custom-form-fields/
- * Description: This plugin demonstrates adds custom fields to your Give Forms.
+ * Description: This plugin demonstrates adds custom fields to your Give give forms with validation, email functionality, and field data output on the payment record within wp-admin.
  * Version: 1.0
  * Author: WordImpress
  * Author URI: https://givewp.com
@@ -30,20 +30,24 @@
  */
 function myprefix123_give_donations_custom_form_fields( $form_id ) {
 
-	// Only display for forms with the IDs "754" and "586";
+	// Only display for forms with the IDs "754" and "578";
 	// Remove "If" statement to display on all forms
 	// For a single form, use this instead:
 	// if ( $form_id == 754) {
-	$forms = array( 754, 586 );
+	$forms = array( 754, 578 );
 
 	if ( in_array( $form_id, $forms ) ) {
 		?>
-		<div id="give-message-wrap">
-			<label class="give-label" for="give-engraving-message"><?php _e( 'What should be engraved on the plaque?:', 'give' ); ?></label>
-			<span class="give-tooltip icon icon-question" data-tooltip="<?php _e( 'Please provide the names that should be engraved on the plaque.', 'give' ) ?>"></span>
+		<div id="give-message-wrap" class="form-row form-row-wide">
+			<label class="give-label"
+				   for="give-engraving-message"><?php _e( 'What should be engraved on the plaque?', 'give' ); ?><?php if ( give_field_is_required( 'give_engraving_message', $form_id ) ) : ?>
+					<span class="give-required-indicator">*</span>
+				<?php endif ?><span class="give-tooltip give-icon give-icon-question"
+									data-tooltip="<?php _e( 'Please provide the names that should be engraved on the plaque.', 'give' ) ?>"></span></label>
+
 			<textarea class="give-textarea" name="give_engraving_message" id="give-engraving-message"></textarea>
 		</div>
-	<?php
+		<?php
 	}
 }
 
@@ -60,108 +64,81 @@ add_action( 'give_after_donation_levels', 'myprefix123_give_donations_custom_for
  */
 function myprefix123_give_donations_require_fields( $required_fields, $form_id ) {
 
-	$required_fields['give-engraving-message'];
+	// Only validate the form with the IDs "754" and "578";
+	// Remove "If" statement to display on all forms
+	// For a single form, use this instead:
+	// if ( $form_id == 754) {
+	$forms = array( 754, 578 );
+	if ( in_array( $form_id, $forms ) ) {
+		$required_fields['give_engraving_message'] = array(
+			'error_id'      => 'invalid_give_engraving_message',
+			'error_message' => __( 'Please enter a message for your engraving', 'give' ),
+		);
+	}
 
 	return $required_fields;
 }
 
 add_filter( 'give_donation_form_required_fields', 'myprefix123_give_donations_require_fields', 10, 2 );
 
-/**
- * Validate Custom Field
- *
- * Check for errors without custom fields.
- *
- * @param $valid_data
- * @param $data
- */
-function myprefix123_give_donations_validate_custom_fields( $valid_data, $data ) {
-
-	// Only validate the form with the IDs "754" and "586";
-	// Remove "If" statement to display on all forms
-	// For a single form, use this instead:
-	// if ( $form_id == 754) {
-	$forms = array( 754, 586 );
-
-	if ( in_array( $data['give-form-id'], $forms ) ) {
-		return;
-	}
-
-	// Check for message data
-	if ( empty( $data['give_message'] ) ) {
-		give_set_error( 'give_message', __( 'Please tell us what you would like inscribed on the plaque.', 'give' ) );
-	}
-}
-
-add_action( 'give_checkout_error_checks', 'myprefix123_give_donations_validate_custom_fields', 10, 2 );
 
 /**
  * Add Field to Payment Meta
  *
  * Store the custom field data custom post meta attached to the `give_payment` CPT.
  *
- * @param $payment
+ * @param $payment_id
  * @param $payment_data
  *
  * @return mixed
  */
-function myprefix123_give_donations_save_custom_fields( $payment, $payment_data ) {
+function myprefix123_give_donations_save_custom_fields( $payment_id, $payment_data ) {
 
-	if( isset( $_POST['give_message'] ) ) {
-
-		$message1 = implode( 'n', array_map( 'sanitize_text_field', explode( 'n', $_POST['give_message'] ) ) );
-		$message2 = sanitize_text_field($_POST['give_message'] );
-
-	     echo "<pre>";
-	    var_dump($message1);
-	    var_dump($message2);
-	    echo "</pre>";
-    }
-
+	if ( isset( $_POST['give_engraving_message'] ) ) {
+		$message = implode( "\n", array_map( 'sanitize_text_field', explode( "\n", $_POST['give_engraving_message'] ) ) );
+		add_post_meta( $payment_id, 'give_engraving_message', $message );
+	}
 
 }
 
-add_action( 'give_insert_payment', array( $this, 'myprefix123_give_donations_save_custom_fields' ), 10, 2 );
+add_action( 'give_insert_payment', 'myprefix123_give_donations_save_custom_fields', 10, 2 );
 
 /**
  * Show Data in Transaction Details
  *
  * Show the custom field(s) on the transaction page.
  *
- * @param $payment_meta
- * @param $user_info
+ * @param $payment_id
  */
-function myprefix123_give_donations_purchase_details( $payment_meta, $user_info ) {
+function myprefix123_give_donations_purchase_details( $payment_id ) {
 
-	// uncomment below to see payment_meta array
-		// echo "<pre>";
-		// var_dump($payment_meta);
-		// echo "</pre>";
-	// Bounce out if no data for this transaction
-	if ( ! isset( $payment_meta['message'] ) ) {
-		return;
-	} ?>
+	$engraving_message = get_post_meta( $payment_id, 'give_engraving_message', true );
 
-	<div class="message-data">
-		<label><?php echo __( 'Message:', 'give' ); ?></label>
-		<?php echo wpautop( $payment_meta['message'] ); ?>
-	</div>
+	if ( $engraving_message ) : ?>
 
-<?php
+		<div id="give-engraving-details" class="postbox">
+			<h3 class="hndle"><?php esc_html_e( 'Engraving Message', 'give' ); ?></h3>
+			<div class="inside" style="padding-bottom:10px;">
+				<?php echo wpautop( $engraving_message ); ?>
+			</div>
+		</div>
+
+	<?php endif;
+
 }
 
-add_action( 'give_payment_personal_details_list', 'myprefix123_give_donations_purchase_details', 10, 2 );
+add_action( 'give_view_order_details_billing_before', 'myprefix123_give_donations_purchase_details', 10, 1 );
 
 /**
  * Adds a Custom "Engraved Message" Tag
  *
- * This function creates a custom Give email template tag
+ * This function creates a custom Give email template tag.
  *
  * @param $payment_id
  */
 function my_custom_prefix_add_sample_referral_tag( $payment_id ) {
 
-	give_add_email_tag( 'Engraved', 'This outputs the Engraved Message', 'my_custom_prefix_get_donation_referral_data' );
+	give_add_email_tag( 'engraving_message', 'This outputs the Engraved Message', 'my_custom_prefix_get_donation_referral_data' );
 
 }
 
@@ -174,19 +151,18 @@ add_action( 'give_add_email_tags', 'my_custom_prefix_add_sample_referral_tag' );
  * The example used here is in conjunction with the Give documentation tutorials.
  *
  * @param $payment_id
+ * @param $payment_meta
  *
- * @return string|void
+ * @return string
  */
 function my_custom_prefix_get_donation_referral_data( $payment_id, $payment_meta ) {
 
-	$payment_meta = give_get_payment_meta( $payment_id );
+	$engraving_message = get_post_meta( $payment_id, 'give_engraving_message', true );
 
-	$output       = __( 'No referral data found.', 'give' );
+	$output = __( 'No referral data found.', 'give' );
 
-	if ( ! empty( $payment_meta['message'] ) ) {
-
-		$output = $payment_meta['message'];
-
+	if ( ! empty( $engraving_message ) ) {
+		$output = wpautop( $engraving_message );
 	}
 
 	return $output;
