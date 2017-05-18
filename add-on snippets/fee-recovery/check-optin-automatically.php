cbfe3fecf530a2e340e4c8936a31443da38cc334
Matt Cromwell <reachme@mattcromwell.com>
2017-05-18 12:51:24 -0700

    Add "Addon" folder, and add new Fee Recovery snippet.

---
+++ add-on snippets/fee-recovery/check-optin-automatically.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *  Check the Donor Opt-in checkbox in Give Fee Recovery automatically
+ * 
+ */
+ 
+function my_give_focus_custom_amount() { ?>
+    <script>
+        jQuery('input.give_fee_mode_checkbox').prop('checked', true);
+    </script>
+
+<?php }
+
+add_action( 'give_post_form_output', 'my_give_focus_custom_amount' );
