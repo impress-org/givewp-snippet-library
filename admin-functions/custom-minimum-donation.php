7dda59fab39260244db24be6df6fe7161dd17795
Devin Walker <sdtucson@gmail.com>
2017-06-27 10:42:01 +0200

    Cleaned up snippet with bad code formatting and using too many params

--- admin-functions/custom-minimum-donation.php
+++ admin-functions/custom-minimum-donation.php
@@ -1,12 +1,14 @@
-<?php 
-
+<?php
 /**
- *  Force a different minimum donation amount and show an error if not met
+ * Force a different minimum donation amount and show an error if not met
+ *
+ * @param $valid_data
+ * @param $posted
  */
- 
- function check_donation_amount($valid_data, $posted) {
-	if ($posted['give-amount'] < 5)
-		give_set_error('donation_amount','The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give');
+function check_donation_amount( $valid_data, $posted ) {
+	if ( $posted['give-amount'] < 5 ) {
+		give_set_error( 'donation_amount', __('The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give' ) );
+	}
 }
 
 add_action( 'give_checkout_error_checks', 'check_donation_amount', 10, 2 );
