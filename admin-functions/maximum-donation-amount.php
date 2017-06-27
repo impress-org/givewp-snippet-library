6b1d920d6381989edafd5d5c9f03d89bcbe1f803
Devin Walker <sdtucson@gmail.com>
2017-06-27 12:28:26 +0200

    New snippet for maximum donation amount

---
+++ admin-functions/maximum-donation-amount.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Prevent donations from a certain amount from submission.
+ *
+ * @param $valid_data
+ * @param $posted
+ *
+ * @return array $valid_data;
+ */
+function give_custom_max_donation_amount( $valid_data, $posted ) {
+
+	// Customize the max amount here.
+	$max_amount = 5000;
+
+	if ( give_sanitize_amount( $posted['give-amount'] ) > $max_amount ) {
+		give_set_error( 'max_donation_amount', sprintf( __( 'The donation amount must be %s or more. Please go back and select a specified amount or enter a lower donation amount.', 'give' ), give_currency_filter( give_format_amount( $max_amount ) ) ) );
+	}
+
+	return $valid_data;
+
+}
+
+add_action( 'give_checkout_error_checks', 'give_custom_max_donation_amount', 10, 2 );
