b79b1936783be1986825e6cadb7878bad14d75b6
Devin Walker <sdtucson@gmail.com>
2017-06-27 12:21:31 +0200

    Must use give_sanitize_amount() to properly compare

--- admin-functions/custom-minimum-donation.php
+++ admin-functions/custom-minimum-donation.php
@@ -6,8 +6,8 @@
  * @param $posted
  */
 function give_custom_min_donation_amount( $valid_data, $posted ) {
-	if ( $posted['give-amount'] < 5 ) {
-		give_set_error( 'donation_amount', __('The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give' ) );
+	if ( give_sanitize_amount( $posted['give-amount'] ) < 5 ) {
+		give_set_error( 'donation_amount', __( 'The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give' ) );
 	}
 }
 
