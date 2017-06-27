99659151b88117140e526e02f62b196cc66b2559
Devin Walker <sdtucson@gmail.com>
2017-06-27 12:28:10 +0200

    Should return $valid_data

--- admin-functions/custom-minimum-donation.php
+++ admin-functions/custom-minimum-donation.php
@@ -4,11 +4,15 @@
  *
  * @param $valid_data
  * @param $posted
+ *
+ * @return array $valid_data
  */
 function give_custom_min_donation_amount( $valid_data, $posted ) {
 	if ( give_sanitize_amount( $posted['give-amount'] ) < 5 ) {
 		give_set_error( 'donation_amount', __( 'The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give' ) );
 	}
+
+	return $valid_data;
 }
 
 add_action( 'give_checkout_error_checks', 'give_custom_min_donation_amount', 10, 2 );
