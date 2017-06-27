c17bd18fa1bd41d33996ca74b561e2c8bb91e705
Devin Walker <sdtucson@gmail.com>
2017-06-27 11:57:41 +0200

    Renamed function to something more applicable

--- admin-functions/custom-minimum-donation.php
+++ admin-functions/custom-minimum-donation.php
@@ -5,10 +5,10 @@
  * @param $valid_data
  * @param $posted
  */
-function check_donation_amount( $valid_data, $posted ) {
+function give_custom_min_donation_amount( $valid_data, $posted ) {
 	if ( $posted['give-amount'] < 5 ) {
 		give_set_error( 'donation_amount', __('The donation amount must be $5 or more. Please go back and select a specified amount or enter a larger donation amount.' ,'give' ) );
 	}
 }
 
-add_action( 'give_checkout_error_checks', 'check_donation_amount', 10, 2 );
+add_action( 'give_checkout_error_checks', 'give_custom_min_donation_amount', 10, 2 );
