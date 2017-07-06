f87c08b38355d05610b4819888e1e75e355e91e4
Devin Walker <sdtucson@gmail.com>
2017-07-06 15:17:49 -0700

    Code reformat

--- customized-templates/add-or-reorder-donation-receipt-items.php
+++ customized-templates/add-or-reorder-donation-receipt-items.php
@@ -15,21 +15,21 @@ function my_give_reorder_donation_receipt_items( $args, $donation_id, $form_id )
 	$updated_args = array();
 
 	// Rearrange the $updated_args based on your need.
-	$updated_args['date']              = $args['date'];
-	$updated_args['donor']             = $args['donor'];
-	$updated_args['donation']          = $args['donation'];
-	$updated_args['donation_id']       = $args['donation_id'];
-    $updated_args['total_donation']    = $args['total_donation'];
-	$updated_args['donation_status']   = $args['donation_status'];
-	$updated_args['payment_details']   = $args['payment_details'];
-	$updated_args['payment_key']       = $args['payment_key'];
-	$updated_args['payment_method']    = $args['payment_method'];
+	$updated_args['date']            = $args['date'];
+	$updated_args['donor']           = $args['donor'];
+	$updated_args['donation']        = $args['donation'];
+	$updated_args['donation_id']     = $args['donation_id'];
+	$updated_args['total_donation']  = $args['total_donation'];
+	$updated_args['donation_status'] = $args['donation_status'];
+	$updated_args['payment_details'] = $args['payment_details'];
+	$updated_args['payment_key']     = $args['payment_key'];
+	$updated_args['payment_method']  = $args['payment_method'];
 
 	// Add new item to the donation receipt.
 	$updated_args['slug'] = array(
-		'name'  => __( 'Label', 'give' ),
-		'value' => 'any custom value',
-		'display' => true	// true or false | whether you need to display the new item in donation receipt or not.
+		'name'    => __( 'Label', 'give' ),
+		'value'   => 'any custom value',
+		'display' => true,// true or false | whether you need to display the new item in donation receipt or not.
 	);
 
 	return $updated_args;
