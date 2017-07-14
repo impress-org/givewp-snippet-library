a7a67da1b1ee68364f646a9ccf3b5557e6e195b9
Devin Walker <sdtucson@gmail.com>
2017-07-13 17:05:18 -0700

    Fixed typo

--- gateway-customizations/customize-payment-gateway-labels.php
+++ gateway-customizations/customize-payment-gateway-labels.php
@@ -27,7 +27,7 @@ function my_custom_gateway_labels( $gateways ) {
 	);
 
 	// add the following 4 lines to change the label for PayPal Payflow.
-	$gateways['paypal_payflow'] = array(
+	$gateways['paypalpro_payflow'] = array(
 		'admin_label'    => 'PayPal Payflow',
 		'checkout_label' => __( 'PayPal Payflow', 'give' ),
 	);
