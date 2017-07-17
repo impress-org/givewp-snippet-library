b6468d3f4bfd75258edf2e19eb3d69180c22ad8d
Matt Cromwell <reachme@mattcromwell.com>
2017-07-17 15:29:16 -0700

    Update give-donation-submenu-show-all-status.php
    Add inline documentation for easy understanding

--- admin-functions/give-donation-submenu-show-all-status.php
+++ admin-functions/give-donation-submenu-show-all-status.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ *   Show All Donation Statuses
+ *   @since Give version 1.8.12
+ *   Shows all donation statuses in sub-menu
+ *   whether donations match that status or not.
+ */
+
 // Check if function does not exists.
 if ( ! function_exists( 'give_payments_table_show_all_status_callback' ) ) {
 	function give_payments_table_show_all_status_callback( $value ) {
