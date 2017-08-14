06c6f72c2296dda437b53f9a3d9c17974791ece8
Matt Cromwell <reachme@mattcromwell.com>
2017-08-14 13:52:27 -0700

    Update add_custom_tags.php

--- add-on-snippets/pdf-receipts/add_custom_tags.php
+++ add-on-snippets/pdf-receipts/add_custom_tags.php
@@ -8,12 +8,15 @@
  
 add_filter('give_pdf_get_template_content', 'give_add_amount_support', 10, 9);
 
-function give_add_amount_support( $template_content ) {
+function give_add_amount_support( $template_content, $payment_id = '6331' ) {
+	$donation_id = give_pdf_get_payment_number( $payment_id );
 
-	$price = give_currency_filter( give_format_amount( 10.50, array( 'sanitize' => false ) ) );
+	$payment     = new Give_Payment( $donation_id );
 
-	$template_content = str_replace( '{amount}', $price, $template_content );
+	$give_amount = give_currency_filter( give_format_amount( $payment->total, array( 'sanitize' => false ) ), $payment->currency );
+
+	$amount = html_entity_decode( $give_amount, ENT_COMPAT, 'UTF-8' );
+	$template_content = str_replace( '{amount}', $amount, $template_content );
 
 	return $template_content;
-    
 }
