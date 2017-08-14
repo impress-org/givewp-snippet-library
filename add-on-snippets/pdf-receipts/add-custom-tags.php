85ca109f212882f5c7cd09a6d5b1e52129e0513a
Devin Walker <sdtucson@gmail.com>
2017-08-14 14:34:47 -0700

    Updated tag snippet to work with fee recovery

--- add-on-snippets/pdf-receipts/add-custom-tags.php
+++ add-on-snippets/pdf-receipts/add-custom-tags.php
@@ -1,14 +1,24 @@
 <?php
 /**
- *  Add a custom tag to be supported in PDF Receipts
+ * Add a custom tag to be supported in PDF Receipts
  *
- *  This tag adds the new {amount} tag
+ * This tag adds the new {amount} tag
+ *
+ * @param $template_content
+ * @param $payment_id
+ *
+ * @return mixed
  */
-function give_add_amount_support( $template_content ) {
+function give_add_amount_support( $template_content, $payment_id ) {
 
-	$donation_id = give_pdf_get_payment_number( $payment_id );
+	// If fee recovery is active, use the replace method there.
+	if ( method_exists( 'Give_Fee_Recovery_Admin', 'give_fee_email_tag_amount' ) ) {
+		$template_content = str_replace( '{amount}', give_fee_recovery()->plugin_admin->give_fee_email_tag_amount( $payment_id ), $template_content );
+		return $template_content;
+	}
 
-	$payment = new Give_Payment( $donation_id );
+	// Fee recovery not available. Output total.
+	$payment = new Give_Payment( $payment_id );
 
 	$give_amount = give_currency_filter( give_format_amount( $payment->total, array( 'sanitize' => false ) ), $payment->currency );
 
@@ -18,5 +28,4 @@ function give_add_amount_support( $template_content ) {
 	return $template_content;
 }
 
-add_filter( 'give_pdf_get_template_content', 'give_add_amount_support', 10, 1 );
-
+add_filter( 'give_pdf_get_template_content', 'give_add_amount_support', 10, 2 );
