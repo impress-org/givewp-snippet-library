cf5f2502f8247ec0c6323bb6b91f55a651c2724a
Devin Walker <sdtucson@gmail.com>
2017-08-14 14:12:14 -0700

    1. Renamed file to match others 2. Moved filter

--- add-on-snippets/pdf-receipts/add-custom-tags.php
+++ add-on-snippets/pdf-receipts/add-custom-tags.php
@@ -1,22 +1,22 @@
 <?php
-
 /**
  *  Add a custom tag to be supported in PDF Receipts
  *
  *  This tag adds the new {amount} tag
  */
- 
-add_filter('give_pdf_get_template_content', 'give_add_amount_support', 10, 9);
+function give_add_amount_support( $template_content ) {
 
-function give_add_amount_support( $template_content, $payment_id = '6331' ) {
 	$donation_id = give_pdf_get_payment_number( $payment_id );
 
-	$payment     = new Give_Payment( $donation_id );
+	$payment = new Give_Payment( $donation_id );
 
 	$give_amount = give_currency_filter( give_format_amount( $payment->total, array( 'sanitize' => false ) ), $payment->currency );
 
-	$amount = html_entity_decode( $give_amount, ENT_COMPAT, 'UTF-8' );
+	$amount           = html_entity_decode( $give_amount, ENT_COMPAT, 'UTF-8' );
 	$template_content = str_replace( '{amount}', $amount, $template_content );
 
 	return $template_content;
 }
+
+add_filter( 'give_pdf_get_template_content', 'give_add_amount_support', 10, 1 );
+
