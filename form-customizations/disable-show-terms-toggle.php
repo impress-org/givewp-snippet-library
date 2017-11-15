e5a870e3a9c5987d29581a4be91272c859253b3d
Devin Walker <sdtucson@gmail.com>
2017-11-14 19:43:56 -0800

    Snippet cleanup

--- form-customizations/disable-show-terms-toggle.php
+++ form-customizations/disable-show-terms-toggle.php
@@ -1,24 +1,28 @@
 <?php
-
-/*
- * Disables the Show Terms toggle link
- * And forces the terms to always appear
+/**
+ * Disables the Show Terms toggle link and forces the terms to always appear.
  */
-
 function disable_give_terms_slidein() {
-	if ( wp_script_is( 'jquery', 'done' ) && is_singular('give_forms') ) { ?>
-		
+
+	// Only if jQuery is present and on single give form. You can customize as needed.
+	if ( wp_script_is( 'jquery', 'done' ) && is_singular( 'give_forms' ) ) { ?>
+
 		<script type="text/javascript">
-			jQuery( "#give_show_terms a" ).removeClass(function($) {
-				return jQuery( this ).attr("class", "show_always");
-			});
+					jQuery( '#give_show_terms a' ).removeClass( function() {
+						return jQuery( this ).attr( 'class', 'show_always' );
+					} );
 		</script>
-		
+
 		<style>
-			#give_show_terms {display: none;}
-			#give_terms {display: block !important;}
+			#give_show_terms {
+				display: none;
+			}
+
+			#give_terms {
+				display: block !important;
+			}
 		</style>
-		
+
 	<?php }
 }
 
