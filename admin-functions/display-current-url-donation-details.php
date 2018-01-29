c35de12d57f967e87b8a125f5994a8166fd68fe0
Devin Walker <sdtucson@gmail.com>
2018-01-29 11:45:20 -0800

    Fixed snippet with missing ".php" file extension and bad doc blocks

--- admin-functions/display-current-url-donation-details.php
+++ admin-functions/display-current-url-donation-details.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
- *  Display the Current URL of the Donation Form
- *  from which the donation was submitted from.
+ * Display the Current URL of the Donation Form from which the donation was submitted from.
+ *
+ * @param int $payment_id
  */
-
-function givecustomer_display_current_url( $payment_id  ) {
+function my_give_customer_display_current_url( $payment_id ) {
 
 	// Bounce out if no data for this transaction
 	$current_url = get_post_meta( $payment_id, '_give_current_url', true );
@@ -21,4 +20,4 @@ function givecustomer_display_current_url( $payment_id  ) {
 
 }
 
-add_action( 'give_payment_view_details', 'givecustomer_display_current_url', 10, 2 );
+add_action( 'give_payment_view_details', 'my_give_customer_display_current_url', 10, 2 );
