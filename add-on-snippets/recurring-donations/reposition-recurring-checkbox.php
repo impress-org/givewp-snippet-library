449de0b7a9fe40d91d5528c9240c9626128c3a81
Devin Walker <sdtucson@gmail.com>
2018-03-05 15:53:23 -0800

    fix(action): update deprecated action

--- add-on-snippets/recurring-donations/reposition-recurring-checkbox.php
+++ add-on-snippets/recurring-donations/reposition-recurring-checkbox.php
@@ -6,16 +6,14 @@
  *
  * This function unhooks Give's checkbox output action and then sets it to a desired location (see https://givewp.com/documentation/developers/actions-explanation-and-usage/ for all available actions) within the donation form using a separate action.
  */
-function my_customize_recurring_checkbox_location(){
+function my_customize_recurring_checkbox_location() {
 
-	//First we remove the action.
-	remove_action( 'give_after_donation_levels', 'give_output_donors_choice_checkbox', 10 );
+	// First we remove the action.
+	remove_action( 'give_after_donation_levels', 'give_output_donors_choice_checkbox', 1 );
 
-
-	//Then we re-add it to our desired location.
+	// Then we re-add it to our desired location.
 	add_action( 'give_before_donation_levels', 'give_output_donors_choice_checkbox', 10, 2 );
-
 
 }
 
-add_action('give_checkout_form_top', 'my_customize_recurring_checkbox_location', 1);
+add_action( 'give_donation_form_top', 'my_customize_recurring_checkbox_location', 1 );
