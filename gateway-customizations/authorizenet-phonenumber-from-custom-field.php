73ffc9473db950d814536d9937d8d75d16fa267c
Devin Walker <sdtucson@gmail.com>
2018-03-19 19:30:28 -0700

    fix(authnet-snippet): updated doc and added opening php tag

--- gateway-customizations/authorizenet-phonenumber-from-custom-field.php
+++ gateway-customizations/authorizenet-phonenumber-from-custom-field.php
@@ -1,7 +1,8 @@
+<?php
 /**
  * Customize Authorize.net payment params. In this case, a phone number from a custom field. Change your_phone_number to the name attribute of the phone field. 
  *
- * Note that this is meant for versionf of The Give Authorize gateway plugin BEFORE 1.3.3, when the gateway code was changed from POST transactions to webhooks. This codes does not work with webhooks.
+ * Note that this is meant for version of The Give Authorize gateway plugin BEFORE 1.3.3, when the gateway code was changed from POST transactions to webhooks. This codes does not work with webhooks.
  * 
  * @param array $args The request that's sent to Authorize.net
  *
