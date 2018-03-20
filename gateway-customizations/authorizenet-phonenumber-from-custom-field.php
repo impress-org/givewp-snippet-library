5ed261354bf70f858ab6906cb84cd2eb9c29bcca
Matt Cromwell <reachme@mattcromwell.com>
2018-03-19 20:51:04 -0700

    Update authorizenet-phonenumber-from-custom-field.php
    Updated description for grammar. Made the action callback and function name more generic.

--- gateway-customizations/authorizenet-phonenumber-from-custom-field.php
+++ gateway-customizations/authorizenet-phonenumber-from-custom-field.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * Customize Authorize.net payment params. In this case, a phone number from a custom field. Change your_phone_number to the name attribute of the phone field. 
+ * Customize Authorize.net payment params. 
+ * 
+ * In this case, a phone number from a custom field. Change your_phone_number to the name attribute of the phone field. 
  *
- * Note that this is meant for version of The Give Authorize gateway plugin BEFORE 1.3.3, when the gateway code was changed from POST transactions to webhooks. This codes does not work with webhooks.
+ * NOTE: This is meant for versions of the Give Authorize.net Add-on BEFORE 1.3.3, when the gateway code was changed from POST transactions to webhooks. This codes does not work with webhooks.
  * 
  * @param array $args The request that's sent to Authorize.net
  *
  * @return array $args
  */
-function guidedogs_authorizenet_phone($args){
+function my_prefix_authorizenet_phone($args){
 	$phone = "";
 	if(! empty( $_POST['your_phone_number'] ) ? $_POST['your_phone_number'] : 'undefined'){
 		$phone = $_POST['your_phone_number'];
@@ -20,4 +22,4 @@ function guidedogs_authorizenet_phone($args){
 
 }
 
-add_filter( 'give_authorize_payment_args', 'guidedogs_authorizenet_phone', 100 );
+add_filter( 'give_authorize_payment_args', 'my_prefix_authorizenet_phone', 100 );
