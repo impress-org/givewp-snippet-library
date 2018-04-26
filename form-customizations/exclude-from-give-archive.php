c92b799f973a95bdd494ee5c4a800935271d0e31
Ben Meredith <ben.meredith@gmail.com>
2018-04-26 12:36:27 -0400

    add comments for clarity.

--- form-customizations/exclude-from-give-archive.php
+++ form-customizations/exclude-from-give-archive.php
@@ -1,12 +1,15 @@
 <?php
 /**
- *  Exclude some Give Forms from the Give Archive page
+ *  Exclude some Give Forms from the Give Archive page. Add form IDs in the array on line 14 (currently 1,13,266)
  */
+
 function my_custom_get_posts( $query ) {
+	//prevent the forms from being hidden on the backend
 	if ( is_admin() ) {
 		return;
 	}
-
+	
+	//limit the query to only Give forms.
 	if ( $query->is_post_type_archive( 'give_forms' ) ) {
 		$query->set( 'post__not_in', array( 1, 13, 266 ) );
 	}
