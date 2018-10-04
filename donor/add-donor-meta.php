8a29cb7080daadc1c1c96f9d030bcda836b3d278
mehul0810 <mehul.gohil0810@gmail.com>
2018-10-04 21:48:43 +0530

    format: improve code and comment

--- donor/add-donor-meta.php
+++ donor/add-donor-meta.php
@@ -2,13 +2,13 @@
 /**
  * Add donor meta field to form
  *
- * @param int $form_id
+ * @param int $form_id Donation Form ID.
  */
-function give_add_donor_phone_form_field( $form_id ){
+function give_add_donor_phone_form_field( $form_id ) {
 	?>
 	<p id="give-email-wrap" class="form-row form-row-wide">
 		<label class="give-label" for="give-email">
-			<?php _e( 'Phone', 'give' ); ?>
+			<?php esc_html_e( 'Phone', 'give' ); ?>
 			<?php if ( give_field_is_required( 'give_phone', $form_id ) ) : ?>
 				<span class="give-required-indicator">*</span>
 			<?php endif ?>
@@ -20,7 +20,7 @@ function give_add_donor_phone_form_field( $form_id ){
 			type="text"
 			name="give_phone"
 			autocomplete="phone"
-			placeholder="<?php _e( 'Phone', 'give' ); ?>"
+			placeholder="<?php esc_html_e( 'Phone', 'give' ); ?>"
 			id="give-email"
 			value="<?php isset( $_POST['give_phone'] ) ? give_clean( $_POST['give_phone'] ) : ''; ?>"
 			required=""
@@ -35,12 +35,13 @@ add_action( 'give_donation_form_after_email', 'give_add_donor_phone_form_field' 
 /**
  * Set donor phone form field as required
  *
- * @param array $required_fields
- * @param int $form_id
+ * @param array $required_fields List of required fields.
+ * @param int   $form_id         Donation Form ID.
  *
  * @return array
  */
 function give_required_donor_phone_form_field( $required_fields, $form_id ){
+
 	$required_fields['give_phone'] = array(
 		'error_id'      => 'invalid_phone',
 		'error_message' => __( 'Please enter phone number.', 'give' ),
@@ -54,16 +55,17 @@ add_action( 'give_donation_form_required_fields', 'give_required_donor_phone_for
  * Save phone number to donation and donor meta
  * Note: donor phone will update in donor meta if donor changes there phone number.
  *
- * @param $donation_id
+ * @param int $donation_id Donation ID.
  */
 function give_save_donor_phone_number( $donation_id ){
-	$donor_id = give_get_payment_donor_id( $donation_id );
+
+	$donor_id         = give_get_payment_donor_id( $donation_id );
 	$new_phone_number = give_clean( $_POST['give_phone'] );
-	$phone_numbers = Give()->donor_meta->get_meta( $donor_id, 'give_phone' );
+	$phone_numbers    = Give()->donor_meta->get_meta( $donor_id, 'give_phone' );
 
 	// Add phone number to donor meta only if not exist.
-	if( ! in_array( $new_phone_number, $phone_numbers ) ) {
-		Give()->donor_meta->add_meta( $donor_id, 'give_phone', $new_phone_number  );
+	if ( ! in_array( $new_phone_number, $phone_numbers, true ) ) {
+		Give()->donor_meta->add_meta( $donor_id, 'give_phone', $new_phone_number );
 	}
 
 	// Save phone number to donation meta.
@@ -74,18 +76,18 @@ add_action( 'give_insert_payment', 'give_save_donor_phone_number', 10 );
 /**
  * Show donor phone numbers on donor profile
  *
- * @param Give_Donor $donor
+ * @param Give_Donor $donor Donor Object.
  */
 function give_show_donor_phone_numbers( $donor ) {
 	$phone_numbers = $donor->get_meta( 'give_phone', false );
 	?>
 	<div id="donor-address-wrapper" class="donor-section clear">
-		<h3><?php _e( 'Phone Numbers', 'give' ); ?></h3>
+		<h3><?php esc_html_e( 'Phone Numbers', 'give' ); ?></h3>
 
 		<div class="postbox">
 			<div class="inside">
 				<?php if ( empty( $phone_numbers ) ) : ?>
-					<?php _e( 'This donor does not have any phone number saved.', 'give' ); ?>
+					<?php esc_html_e( 'This donor does not have any phone number saved.', 'give' ); ?>
 				<?php else: ?>
 					<?php foreach ( $phone_numbers as $phone_number ) : ?>
 						<?php echo $phone_number; ?><br>
