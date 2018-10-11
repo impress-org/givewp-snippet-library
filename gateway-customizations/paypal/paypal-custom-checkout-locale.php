b9627648fc73f49b88ff731e28d8841f3a72a861
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-10-11 11:14:20 -0400

    fix: filter PayPal args for recurring donations

--- gateway-customizations/paypal/paypal-custom-checkout-locale.php
+++ gateway-customizations/paypal/paypal-custom-checkout-locale.php
@@ -4,17 +4,16 @@
  *
  * For locales see https://developer.paypal.com/docs/classic/api/locale_codes/
  *
- * @param array $paypal_args   Params used to build redirect query to PayPal.
+ * @param array $paypal_args   Arguments used to build redirect query to PayPal.
  * @param array $donation_data Information about the donation.
  *
- * @return mixed
+ * @return array Modified array of PayPal arguments.
  */
 function give_customize_paypal_standard_checkout_language( $paypal_args, $payment_data ) {
-
 	$paypal_args['lc'] = 'pt_BR';
 
 	return $paypal_args;
-
 }
 
-add_filter( 'give_paypal_redirect_args', 'give_customize_paypal_standard_checkout_language', 99, 2 );
+add_filter( 'give_paypal_redirect_args', 'give_customize_paypal_standard_checkout_language', 99, 2 ); // Filter args for one-time donations.
+add_filter( 'give_recurring_paypal_args', 'give_customize_paypal_standard_checkout_language', 99, 2 ); // Filter args for recurring donations.
