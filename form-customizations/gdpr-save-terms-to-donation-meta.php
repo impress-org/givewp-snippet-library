28b57fee720d6855a79780bef621d3d90f38833d
Matt Cromwell <reachme@mattcromwell.com>
2019-03-19 16:43:42 -0700

    Create gdpr-save-terms-to-donation-meta.php

---
+++ form-customizations/gdpr-save-terms-to-donation-meta.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *  Add Terms Agreement text and consent to Donation Meta
+ *  NOTE: This is then used in the Give CSV Export to show consent.
+ *
+ */
+
+/**
+ * Output Terms in hidden textarea for saving
+ *
+ * @param $form_id
+ */
+function custom_givewp_output_terms_for_saving($form_id) {
+
+    $formmeta = get_post_meta($form_id);
+    $terms_enabled = $formmeta['_give_terms_option'][0];
+    $terms = $formmeta['_give_agree_text'][0];
+
+    if ( $terms_enabled==='enabled' ) {
+        ?>
+        <textarea class="give-textarea" name="give_form_terms" id="give_form_terms" aria-hidden="true"
+                  style="display:none;opacity: 0; height: 0; border: 0 !important;"><?php echo $terms; ?></textarea>
+        <?php
+    }
+
+}
+
+add_action('give_after_donation_levels', 'custom_givewp_output_terms_for_saving');
+
+
+/**
+ * Save Terms and Terms Agreement in to donation meta
+ *
+ * @param $form_id
+ */
+
+function custom_give_save_terms_agreement_to_donation_paymentmeta( $payment_id ) {
+    global $post;
+
+    if ( isset($_POST['give_agree_to_terms']) ) {
+        $agreed = wp_strip_all_tags( $_POST['give_agree_to_terms'], true );
+        $terms = wp_strip_all_tags( $_POST['give_form_terms'], true );
+        $pretty_terms = ( $terms == 1 ) ? __('Yes', 'give') : __('No','give');
+
+        give_update_payment_meta( $payment_id, 'give_form_terms', $terms );
+        give_update_payment_meta( $payment_id, 'give_agree_to_terms', $agreed );
+
+    }
+}
+
+add_action('give_insert_payment', 'custom_give_save_terms_agreement_to_donation_paymentmeta');
