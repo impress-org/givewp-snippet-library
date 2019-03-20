5bbb7fb4617d93c78b17fabb47181070eeaaf772
Matt Cromwell <reachme@mattcromwell.com>
2019-03-20 11:15:45 -0700

    Update gdpr-save-terms-to-donation-meta.php
    Add logic to switch to Global setting if "Global" is enabled on the form.

--- form-customizations/gdpr-save-terms-to-donation-meta.php
+++ form-customizations/gdpr-save-terms-to-donation-meta.php
@@ -15,9 +15,15 @@ function custom_givewp_output_terms_for_saving($form_id) {
 
     $formmeta = get_post_meta($form_id);
     $terms_enabled = $formmeta['_give_terms_option'][0];
-    $terms = $formmeta['_give_agree_text'][0];
+    $global = give_get_settings();
 
-    if ( $terms_enabled==='enabled' ) {
+    switch($terms_enabled) {
+        case 'enabled': $terms = $formmeta['_give_agree_text'][0]; break;
+        case 'global': $terms = $global['agreement_text']; break;
+        default: return;
+    }
+
+    if ( $terms_enabled!=='disabled' ) {
         ?>
         <textarea class="give-textarea" name="give_form_terms" id="give_form_terms" aria-hidden="true"
                   style="display:none;opacity: 0; height: 0; border: 0 !important;" readonly aria-readonly="true" disabled aria-disabled="true"><?php echo $terms; ?></textarea>
@@ -43,10 +49,11 @@ function custom_give_save_terms_agreement_to_donation_paymentmeta( $payment_id )
         $terms = wp_strip_all_tags( $_POST['give_form_terms'], true );
         $pretty_terms = ( $terms == 1 ) ? __('Yes', 'give') : __('No','give');
 
-        give_update_payment_meta( $payment_id, 'give_form_terms', $terms );
+        give_update_payment_meta( $payment_id, 'give_form_terms', $pretty_terms );
         give_update_payment_meta( $payment_id, 'give_agree_to_terms', $agreed );
 
     }
 }
 
 add_action('give_insert_payment', 'custom_give_save_terms_agreement_to_donation_paymentmeta');
+
