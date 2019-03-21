26a7242fd40686b69103c6790c718a24d1c662bf
Ben Meredith <ben.meredith@gmail.com>
2019-03-21 17:52:08 -0400

    update PDF tags snippet to support new table-only templates.

--- add-on-snippets/pdf-receipts/add-custom-tags.php
+++ add-on-snippets/pdf-receipts/add-custom-tags.php
@@ -2,30 +2,23 @@
 /**
  * Add a custom tag to be supported in PDF Receipts
  *
- * This tag adds the new {amount} tag
+ * This tag adds a new {total} tag which displays a total without breaking it down 
+ * if Fee Recovery is enabled.
  *
  * @param $template_content
- * @param $payment_id
+ * @param $args
  *
  * @return mixed
  */
-function give_add_amount_support( $template_content, $payment_id ) {
-
-	// If fee recovery is active, use the replace method there.
-	if ( method_exists( 'Give_Fee_Recovery_Admin', 'give_fee_email_tag_amount' ) ) {
-		$template_content = str_replace( '{amount}', give_fee_recovery()->plugin_admin->give_fee_email_tag_amount( $payment_id ), $template_content );
-		return $template_content;
-	}
-
-	// Fee recovery not available. Output total.
-	$payment = new Give_Payment( $payment_id );
-
-	$give_amount = give_currency_filter( give_format_amount( $payment->total, array( 'sanitize' => false ) ), $payment->currency );
-
-	$amount           = html_entity_decode( $give_amount, ENT_COMPAT, 'UTF-8' );
-	$template_content = str_replace( '{amount}', $amount, $template_content );
-
+ 
+function give_add_total_pdf_tag( $template_content, $args ) {
+	
+	// during testing, uncomment the next line to see a full printed array of the possible args that you can query
+	// var_dump("<pre>".print_r($args,true)."</pre>");
+	
+	$total     = isset( $args['payment_meta']['_give_payment_total'] ) ? html_entity_decode( give_currency_filter( give_format_amount( $args['payment_meta']['_give_payment_total'] ) ), ENT_COMPAT, 'UTF-8' ) : '';
+	$template_content = str_replace( '{total}', $total, $template_content );
 	return $template_content;
 }
 
-add_filter( 'give_pdf_get_template_content', 'give_add_amount_support', 10, 2 );
+add_filter( 'give_pdf_compiled_template_content', 'give_add_total_pdf_tag', 999, 2 );
