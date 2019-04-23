7e42bdf5420a718b6e7e25e9a9a14703f6da295a
Matt Cromwell <reachme@mattcromwell.com>
2019-04-23 14:44:28 -0700

    Update custom-fee-recovery-breakdown-language.php

--- translation-snippets/custom-fee-recovery-breakdown-language.php
+++ translation-snippets/custom-fee-recovery-breakdown-language.php
@@ -4,6 +4,8 @@
  *  Change the Fee Recovery summary language at the bottom of the form
  *  when a donor has opted in to cover the fees
  *
+ *  {fee_amount} and {amount} are populated dynamically
+ *
  **/
 
 add_filter('give_fee_break_down_message', 'my_custom_give_fee_language');
