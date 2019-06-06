eca06ca23c57f255c0d1604921ddf50b17f17ff7
mehul0810 <mehul.gohil0810@gmail.com>
2019-06-06 15:21:04 +0530

    fix: update code to make snippet work

--- gateway-customizations/offline-auto-complete.php
+++ gateway-customizations/offline-auto-complete.php
@@ -7,19 +7,21 @@
  * @param array $payment_args
  * @param array $payment_data
  *
- * @return array @$payment_args
+ * @return array $payment_args
  */
 function give_auto_approve_offline_donations( $payment_args, $payment_data ) {
 
-	// Ensure gateway is set and check for offline gateway
-	if ( ! isset( $payment_data['gateway'] ) || $payment_data['gateway'] !== 'offline' ) {
-		// Passthrough
+	$payment_mode = isset( $_GET['payment-mode'] ) ? give_clean( $_GET['payment-mode'] ) : false;
+	
+	// Ensure gateway is set and check for offline gateway.
+	if ( 'offline' !== $payment_mode ) {
+		// Passthrough.
 		return $payment_args;
 	}
 
-	// Only pending donations
+	// Only pending donations.
 	if ( isset( $payment_args['post_status'] ) && $payment_args['post_status'] === 'pending' ) {
-		// Set post_status to complete / publish
+		// Set post_status to complete / publish.
 		$payment_args['post_status'] = 'publish';
 	}
 
