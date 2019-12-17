e2049ef1fd6548d751c314ec85e206227bc1ac4b
Devin Walker <sdtucson@gmail.com>
2019-12-17 15:31:31 -0800

    fix: resolve issue with auto-complete not affecting the goal amount because of hooking in to wrong place

--- gateway-customizations/offline-auto-complete.php
+++ gateway-customizations/offline-auto-complete.php
@@ -1,32 +1,26 @@
 <?php
 /**
- * Give Auto Approve Offline Donations
+ * Auto Approve Offline Donations
  *
- * If you would like to have donations skip the "pending" stage and auto-approve
+ * If you would like to have donations skip the "pending" stage and auto-approve and set them to "complete" status.
  *
- * @param array $payment_args
  * @param array $payment_data
  *
- * @return array $payment_args
+ * @return mixed
  */
-function give_auto_approve_offline_donations( $payment_args, $payment_data ) {
+function give_auto_approve_offline_donations( $payment_data ) {
 
-	$payment_mode = isset( $_GET['payment-mode'] ) ? give_clean( $_GET['payment-mode'] ) : false;
-	
+	$gateway    = ! empty( $payment_data['gateway'] ) ? $payment_data['gateway'] : '';
+	$gateway    = empty( $gateway ) && isset( $_POST['give-gateway'] ) ? give_clean( $_POST['give-gateway'] ) : $gateway;
+
 	// Ensure gateway is set and check for offline gateway.
-	if ( 'offline' !== $payment_mode ) {
-		// Passthrough.
-		return $payment_args;
+	if ( 'offline' === $gateway ) {
+		$payment_data['status'] = 'publish';
 	}
 
-	// Only pending donations.
-	if ( isset( $payment_args['post_status'] ) && $payment_args['post_status'] === 'pending' ) {
-		// Set post_status to complete / publish.
-		$payment_args['post_status'] = 'publish';
-	}
-
-	return $payment_args;
+	// Always return $payment_data back.
+	return $payment_data;
 
 }
 
-add_filter( 'give_insert_payment_args', 'give_auto_approve_offline_donations', 999, 2 );
+add_action( 'give_pre_insert_payment', 'give_auto_approve_offline_donations' );
