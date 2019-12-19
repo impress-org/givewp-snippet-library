a46e2fd504fa37b82cb5204aeb7f2becd9028752
Devin Walker <sdtucson@gmail.com>
2019-12-18 21:56:53 -0800

    refactor: fix snippet and provide huge warning
    Resolves #100

--- form-customizations/customize-fieldset-order.php
+++ form-customizations/customize-fieldset-order.php
@@ -1,26 +1,32 @@
 <?php
 /**
- * Reorder fieldsets within the Give donation form
+ * Reorder fieldsets within the Give donation form.
  *
+ * WARNING: This snippet can be problematic if running older versions of GiveWP and multiple payment gateways.
+ * Whenever unhooking and rehooking always TEST THOROUGHLY and never code on a live site.
  */
 
-/* Remove all Give Fieldsets */
+/**
+ * Remove all Give Fieldsets.
+ */
 function give_remove_fieldsets() {
-    remove_action( 'give_donation_form_top', 'give_output_donation_amount_top' );
-    remove_action( 'give_payment_mode_select', 'give_payment_mode_select' );
-    remove_action( 'give_donation_form_after_user_info', 'give_user_info_fields' );
-    remove_action( 'give_cc_form', 'give_get_cc_form' );
-    remove_all_actions( 'give_donation_form_after_cc_form' );
+	remove_action( 'give_donation_form_top', 'give_output_donation_amount_top' );
+	remove_action( 'give_payment_mode_select', 'give_payment_mode_select' );
+	remove_action( 'give_donation_form_after_user_info', 'give_user_info_fields' );
+	remove_action( 'give_cc_form', 'give_get_cc_form' );
+	remove_all_actions( 'give_donation_form_after_cc_form' );
 }
 
-add_action( 'init', 'give_remove_fieldsets' );
+add_action( 'give_init', 'give_remove_fieldsets' );
 
-/* Add all Give Fieldsets */
+/**
+ * Add all Give Fieldsets in another order.
+ */
 function give_reorder_fieldsets() {
-    add_action( 'give_donation_form_top', 'give_user_info_fields' );
-    add_action( 'give_donation_form_top', 'give_output_donation_amount_top' );
-    add_action( 'give_donation_form_top', 'give_payment_mode_select' );
-    add_action( 'give_donation_form_top', 'give_checkout_submit' );
+	add_action( 'give_donation_form_top', 'give_output_donation_amount_top' );
+	add_action( 'give_donation_form_top', 'give_user_info_fields' );
+	add_action( 'give_donation_form_top', 'give_payment_mode_select', 10, 2 );
+	add_action( 'give_donation_form_bottom', 'give_checkout_submit', 9999, 2 );
 }
 
-add_action( 'init', 'give_reorder_fieldsets' );
+add_action( 'give_init', 'give_reorder_fieldsets' );
