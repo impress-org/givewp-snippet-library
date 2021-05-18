35c82b78caa107b5b7c9b6f239d59e9ac4b9589d
Matheus Martins <matheusdealmeidamartins@gmail.com>
2021-05-18 14:07:17 +0100

    Updates the script to work with donor dashboard too
    Adds the information about how to style the multi-step donation form and the donor dashboard. Instead of creating different code snippets, we can keep only this one that works for both of them.

---
+++ form-customizations/css-customizations/style-givewp-iframes.php
@@ -0,0 +1,31 @@
+<?php 
+
+/**
+ *  The multi-Step form template and the donor dashboard load in an iframe, which prevents theme styles from interfering with their styles.
+ *  To style them, use this PHP snippet to add inline styles. Replace lines 13-17 with your custom styles.
+ */
+
+function override_iframe_template_styles_with_inline_styles() {
+    wp_add_inline_style(
+        /**
+         *  Below, use give-sequoia-template-css to style the multi-step donation form
+         *  or use give-donor-dashboards-app to style the donor dashboard
+         */
+        'give-sequoia-template-css',
+        '
+        /* add styles here! A sample (turns the headline text blue): */
+
+        .introduction .headline {
+            color: blue;
+        }
+        
+        /* It changes the donor name on the donor dashboard to green: */
+        
+        .give-donor-dashboard-donor-info__details .give-donor-dashboard-donor-info__name {
+            color: green;
+        }
+        '
+    );
+}
+
+add_action('wp_print_styles', 'override_iframe_template_styles_with_inline_styles', 10);
